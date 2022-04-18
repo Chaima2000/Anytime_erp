6c5a7eadfb21d43c127c9ed7cbbbcc0e95b755c0
Chaima2000 <schaima2019@gmail.com>
2022-04-18 12:15:00 +0100

    53%

--- OneDrive/Bureau/Anytime-Anywhere_ERP-master/client/src/Pages/authentication/Login.js
+++ OneDrive/Bureau/Anytime-Anywhere_ERP-master/client/src/Pages/authentication/Login.js
@@ -1,12 +1,13 @@
-import styles from "../../Css/Login.module.css";
+import styles from  "../../Css/Login.module.css";
 import { Link, useHistory } from "react-router-dom";
 import Modal from 'react-modal';
-import { useState, useContext } from "react";
+import { useState, useContext , useEffect , useRef } from "react";
 import { AppContext } from "../../Context/AppContext";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
-
+import {TweenMax} from "gsap";
+import img1 from "../../uploads/img1.png";
 
 function Login() {
   const history = useHistory();
@@ -14,6 +15,19 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [waiting, setWaiting] = useState(null);
+   
+  let imgs = useRef(null);
+  let headers = useRef(null);
+  let logos = useRef(null);
+  let form = useRef(null);
+
+  useEffect ( () => {
+    TweenMax.to(imgs,1,{delay:0.2 , opacity: 1 , ease : "easeOut"})
+    TweenMax.to(logos,2,{delay:0.2 , opacity: 1 , ease : "easeOut"})
+    TweenMax.to(headers,2,{delay:0.3 , opacity: 1 , ease : "easeOut"})
+    TweenMax.to(form,2,{delay:0.3 , opacity: 1 , ease : "easeOut"})
+  })
+
 
 
 
@@ -41,50 +55,43 @@ function Login() {
 
   return (
     <>
-                
-      {/* <section >
-        <div className="card">
-          <h1>Login</h1>
-          <hr />
-          <form onSubmit={login}>
-            <input
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              required
-              className="formInput"
-              placeholder="Email"
-              type="email"
-            />
-            <input
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              required
-              className="formInput"
-              placeholder="Password"
-              type="password"
-            />
-            {waiting ? (
-              <FontAwesomeIcon icon={solid("spinner")} spin size="2x" />
-            ) : (
-              <>
-                <button className="defaultBtn">Login</button>
-              </>
-            )}
-            <div align="right">
-              <Link to="/forgotpassword">Forgot password ?</Link>
-            </div>
-            <div style={{ paddingTop: "40px" }} align="left">
-              <Link to="/signup">Don't have an account ? create one !</Link>
-            </div>
-          </form>
-        </div>
-      </section> */}
-
-
-      
-
+       <div className={styles.wrapper}>
+          <div className={styles.separate} id="start">
+             <div className={styles.banner} ref={el => imgs = el}>
+                <img src={img1} alt="main-img" className={styles.banner_img} />
+             </div>
+             <p className={styles.copyright}>© 2021 A&A.  All rights reserved</p>
+          </div>
+          <div className={styles.form_section}>
+             <div className={styles.form_style}>
+                <div className={styles.logo} ref={el => logos = el}>
+                <img src={process.env.PUBLIC_URL + "/logo.png"} />
+                </div>
+                <h2 ref={el => headers = el}>Welcome to A&A</h2>
+                <form ref={el => form = el} className={styles.form} onSubmit={login}>
+                    <div className={styles.fields}>
+                      <label> Email</label><br />
+                      <input type="email" placeholder="Enter your email" onChange={(e) => {setEmail(e.target.value);}} required />
+                    </div>
+                    <div className={styles.fields}>
+                      <label>Password</label><br />
+                      <input type="password" placeholder="Enter your password" onChange={(e) => { setPassword(e.target.value)}} required />
+                    </div>
+                    {waiting ? (
+                          <FontAwesomeIcon icon={solid("spinner")}  spin size="2x" className={styles.spinner} />
+                        ) : (
+                          <>
+                          <button type="submit" className={styles.submit_btn}> Login </button>
+                          </>
+                        )}
+                    <br />
+                    <Link to="/forgotpassword"  className={styles.p_right}><p>Forgot Password?</p></Link>
+                    <br />
+                    <Link to="/signup" className={styles.p_left}>Don't have an account ? create one !</Link>
+                </form>
+             </div>
+          </div>
+      </div>
     </>
   );
 }

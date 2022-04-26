ade92fec82d24859240dab251276a84e0f6a11f3
Chaima2000 <schaima2019@gmail.com>
2022-04-26 18:32:06 +0100

    60%

--- OneDrive/Bureau/Anytime-Anywhere_ERP-master/client/src/Pages/navigation/Contact.js
+++ OneDrive/Bureau/Anytime-Anywhere_ERP-master/client/src/Pages/navigation/Contact.js
@@ -1,16 +1,46 @@
-import React, { useMemo } from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-
+import React , {useState} from 'react';
+import styles from "../../Css/Contact.module.css";
+import emailjs from 'emailjs-com';
+const Result = () => {
+  return ( 
+    <p className={styles.showMsg}>Your message has been successfully sent. <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We will contact you soon.</p>
+  )
+}
 function Contact() {
-  const { isLoaded} = useLoadScript({
-    googleMapsApiKey : process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,  });
-    if( !isLoaded) return <div>Loading ...</div>;
-    function Map(){
-      return <GoogleMap zoom={10} center={ {lat: 44, lng: -80 }} ></GoogleMap>
-    }
+  const [result, showResult] = useState(false);
+  const contactForm = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_xtc8ywt', 'template_4rs1ggn', e.target, "bRBjGAR1Tjp0ZQSZO")
+  .then ( (result) => {
+    console.log(result.text);
+  } , (error) => {
+    console.log(error.text);
+  });
+  e.target.reset();
+  showResult(true);
+  };
+  //Hide showMsg
+  setTimeout( ()=> {
+    showResult(false);
+  }, 5000);
+  
   return (
-    <Map />
-  );
+    <form className={styles.form} onSubmit={contactForm}>
+      <h2 align="center">Contact Us</h2>
+      <div className={styles.input_section}>
+        <input  type="text" placeholder='Enter your full name' name="fullName" className={styles.input} /> <br /> <br />
+        <input type="email" placeholder='Enter your Email' name="email" className={styles.input} /> <br /> <br />
+        <input  type="number" placeholder='Enter your phone' name="phone" className={styles.input} /> <br /> <br />
+        <textarea placeholder ='Enter your message' name="message" className={styles.textarea} /> <br /> <br />
+      </div>
+      <button className={styles.btn}> Send </button>
+      <div>
+        {result ? <Result/> : null}
+      </div>
+    </form>
+  )
 }
 
-export default Contact;
+export default Contact
+
+
